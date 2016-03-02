9a3ad63bc068d4ccd2e9a2ba99de1f76ce4666d5
buschidos <sergey.snjozyk@gmail.com>
2016-03-02 18:36:01 +0200

    slider

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 $(function() {
-    var root = $('body');
-    var openedStack = [];
 
+    // dropdowns
     $('[data-trigger]').on('click', function(e) {
         var _this = $(this);
         var parent = _this.parents('.drop');
@@ -10,4 +9,107 @@ $(function() {
 
         parent.toggleClass('is-open');
     });
+
+    // attendance slider
+    var container = $('.attendance');
+    var slider = container.find('.slider');
+    var tooltip = container.find('.slider-tooltip').remove();
+    var displayedValue = container.find('.attendance__value');
+    var values = [50, 100, 250, 500, 1000, 3000, 5000, 10000, 25000, 30000];
+    var breakpoints = [50, 250, 1000, 5000];
+    var currentBpIndex;
+    var CLASS_TEST = /bp-\d/g;
+
+    container.find('.slider-points').html(renderDots(values));
+
+    noUiSlider.create(slider[0], {
+        start: values[0],
+        range: createRange(values)
+    });
+
+    slider.find('.noUi-handle-lower').append(tooltip);
+
+    slider[0].noUiSlider.on('change', onSliderChange);
+    slider[0].noUiSlider.on('set', onSliderChange);
+    slider[0].noUiSlider.set(values[0]);
+
+    function onSliderChange(strVal, handle, val, tap, positions) {
+        var value = val[handle];
+        var newBpIndex = getBpIndex(value);
+        console.log(newBpIndex);
+        console.log(strVal, handle, val, tap, positions);
+
+        displayedValue.text(formatValueSting(val[handle], values));
+
+        if (newBpIndex !== currentBpIndex) {
+            tooltip
+                .removeClass(function(i, className) {
+                    var matched = className.match(CLASS_TEST);
+                    if (matched) return matched.join(' ');
+                    return '';
+                })
+                .addClass('bp-' + (newBpIndex + 1));
+        }
+    }
+
+    function createRange(values) {
+        var range = {};
+        var step = Math.round(100 / (values.length - 1) * 100) / 100;
+        values.forEach(function(val, i) {
+            switch (i) {
+                case 0:
+                    range.min = [val, values[i + 1] - val];
+                    break;
+                case values.length - 1:
+                    range.max = [val];
+                    break;
+                default:
+                    range[i * step + '%'] = [val, values[i + 1] - val];
+            }
+        });
+        return range;
+    }
+
+    function renderDots(values) {
+        var step = Math.round(100 / (values.length - 1) * 100) / 100;
+
+        return values.map(function(val, i) {
+            return (
+                '<div class="slider-point" style="left: ' + (step * i) + '%">'
+                    + formatValueSting(val, values)
+                + '</div>'
+            );
+        }).join('');
+    }
+
+    function formatValueSting(value, array) {
+        var index = array.indexOf(value);
+        switch (index) {
+            case -1:
+                return formatNumber(value);
+            case array.length - 1:
+                return formatNumber(array[index - 1]) + '+';
+            default:
+                return formatNumber(array[index - 1] || 0) + '-' + formatNumber(value);
+        }
+    }
+
+    function formatNumber(str) {
+        if (typeof str !== 'string') str = str.toString();
+        return str
+            .split('')
+            .reverse()
+            .map(function(el, i) {
+                return (i === 0 || i % 3) ? el : el + ',';
+            })
+            .reverse()
+            .join('');
+    }
+
+    function getBpIndex(val) {
+        for (var i = 0; i < breakpoints.length; i++) {
+            if (val >= breakpoints[i] && val < (breakpoints[i + 1] || Infinity)) return i;
+        }
+        return 0;
+    }
 });
